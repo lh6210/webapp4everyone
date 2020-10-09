d419b3b19178499637228d65eee43c4d3aac5360
hang li <lh6210@gmail.com>
2020-10-09 09:14:57 -0600

    oct-9

--- route/add.php
+++ route/add.php
@@ -21,14 +21,14 @@ if (isset($_POST['cancel'])) {
 
 // add a new record, validate and redirect with a message
 if (isset($_POST['add'])) {
-	if (empty($_POST['Make'])) {
+	if (empty($_POST['make'])) {
 		echo 'Make is required';
 	} elseif (!is_numeric($_POST['mileage']) || !is_numeric($_POST['year'])) {
 		echo 'Mileage and year must be numeric.';
 	}
 	else {
 		$sql = "insert into autos (make, year, mileage) values (:make, :year, :mileage)";
-		$data = array(':make'=> htmlentities($_POST['Make']), ':year'=>htmlentities($_POST['year']), ':mileage'=>htmlentities($_POST['mileage']));
+		$data = array(':make'=> htmlentities($_POST['make']), ':year'=>htmlentities($_POST['year']), ':mileage'=>htmlentities($_POST['mileage']));
 		$stmt = $pdo->prepare($sql);
 		$stmt->execute($data);
 
@@ -50,7 +50,7 @@ if (isset($_POST['add'])) {
 <h1>Tracking Autos for <?= $_SESSION['who'] ?></h1>
 <form method='POST'>
 <label for='make'>Make:</label><br>
-<input type='text' name='Make' id='make'><br>
+<input type='text' name='make' id='make'><br>
 <label for='yr'>Year:</label><br>
 <input type='text' name='year' id='yr'><br>
 <label for='miles'>Mileage:</label><br>
